c7c2adda74401f70b1cf160da329561f7acccc71
NevilleJS <nevillejs24@gmail.com>
2021-09-20 12:48:13 +0530

    5head

--- stock.py
+++ stock.py
@@ -4,6 +4,7 @@ import csv
 # def UpdateDB(from_db,to_db):
     # Update stock from existing db
     # Abhi and John
+
 # Exit code 0: no error, 1: does not exist, 2: insufficent qty
 def ItemAdd(id,qty,db):
     if id in db.keys():
@@ -27,7 +28,7 @@ def ReadStockFile(file):
         reader = csv.reader(csvfile)
         next(reader)
         for i in reader:
-            data.append([j.strip() for j in i])
+            data.append([int(j.strip()) if j.strip().isnumeric() and j!=i[0] else j.strip() for j in i])
     for i in data:
-        data_dict[i[0]] = [i[1],int(i[2]),int(i[3])]
+        data_dict[i[0]] = [i[1],i[2],i[3]]
     return data_dict
