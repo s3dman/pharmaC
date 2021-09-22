670f0c37a0fa525d4dfeb8d015c3deace0218071
NevilleJS <nevillejs24@gmail.com>
2021-09-22 09:33:13 +0530

    handling io errors

--- main.py
+++ main.py
@@ -2,15 +2,23 @@ import pickle
 
 # To read and write data into .DB files
 def WriteDB(db,file):
-    db_file = open(f'./database/{file}','wb')
-    pickle.dump(db,db_file)
-    db_file.close()
+    try:
+        db_file = open(f'./database/{file}','wb')
+        pickle.dump(db,db_file)
+        db_file.close()
+        return 0
+    except IOError:
+        return 1
+
 
 def ReadDB(file):
-    db_file = open(f'./database/{file}','rb')
-    db = pickle.load(db_file)
-    db_file.close()
-    return db
+    try:
+        db_file = open(f'./database/{file}','rb')
+        db = pickle.load(db_file)
+        db_file.close()
+        return db
+    except IOError:
+        return 1
 
 def PP(db):
     for i in db:
