034c1e4b0cf81c08602ef364e9c79089a2005805
NevilleJS <nevillejs24@gmail.com>
2021-09-24 11:32:11 +0530

    bruh

--- ui.py
+++ ui.py
@@ -1,6 +1,19 @@
-import tkinter as tk
-root = tk.Tk()
-# root.geometry("450*800")
-root.minsize(800,450)
-root.maxsize(800,450)
-root.mainloop()
+from main import *
+
+def tabulate(header,data,printheader=True,linesbetweenrows=False,prependspace=0):
+    widths = [len(cell) for cell in header]
+    for row in data:
+        for i, cell in enumerate(row):
+            widths[i] = max(len(str(cell)), widths[i])
+    formatted_row = ' | '.join('{:%d}' % width for width in widths)
+    if printheader:
+        print(prependspace*' '+'+'+'-'*(len(formatted_row.format(*header))+2)+'+')
+        print(prependspace*' '+'| '+formatted_row.format(*header)+' |')
+        print(prependspace*' '+'+'+'='*(len(formatted_row.format(*header))+2)+'+')
+    else:
+        print(prependspace*' '+'+'+'-'*(len(formatted_row.format(*header))+2)+'+')
+    for row in data:
+        print(prependspace*' '+'| '+formatted_row.format(*row)+' |')
+        if linesbetweenrows:
+            print(prependspace*' '+'+'+'-'*(len(formatted_row.format(*header))+2)+'+')
+    if linesbetweenrows == False: print(prependspace*' '+'+'+'-'*(len(formatted_row.format(*header))+2)+'+')
