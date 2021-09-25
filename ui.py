510b4432b3bbb8823b0ee4952145c8d600454518
NevilleJS <nevillejs24@gmail.com>
2021-09-25 10:20:19 +0530

    made startup pages for ui.py

--- ui.py
+++ ui.py
@@ -1,4 +1,6 @@
 from main import *
+import time
+import os
 
 def tabulate(header,data,printheader=True,linesbetweenrows=False,prependspace=0):
     widths = [len(cell) for cell in header]
@@ -17,3 +19,32 @@ def tabulate(header,data,printheader=True,linesbetweenrows=False,prependspace=0)
         if linesbetweenrows:
             print(prependspace*' '+'+'+'-'*(len(formatted_row.format(*header))+2)+'+')
     if linesbetweenrows == False: print(prependspace*' '+'+'+'-'*(len(formatted_row.format(*header))+2)+'+')
+
+Prepend = ' '*8
+
+def LoadingScreen():
+    for i in range(58):
+        print(f"""
+{Prepend} ____   _____  ____      ____   _
+{Prepend}/ ___| | ____||  _ \    |  _ \ | |__    __ _  _ __  _ __ ___    __ _
+{Prepend}\___ \ |  _|  | | | |   | |_) || '_ \  / _` || '__|| '_ ` _ \  / _` |
+{Prepend} ___) || |___ | |_| |   |  __/ | | | || (_| || |   | | | | | || (_| |
+{Prepend}|____/ |_____||____/    |_|    |_| |_| \__,_||_|   |_| |_| |_| \__,_|\n\n""")
+        print(f"{Prepend}LOADING: [{'#'*i+' '*(58-i)}]")
+        time.sleep(0.05)
+        os.system('clear')
+    HomePage()
+
+def HomePage():
+    print("Type the option you want to choose and press Enter:\n")
+    tabulate(
+        "Option Service".split(),
+        [
+            "1 Billing".split(),
+            "2 Inventory".split(),
+            "3 Management".split(),
+            "0 Quit".split(),
+        ],
+        linesbetweenrows=True,
+    )
+LoadingScreen()
