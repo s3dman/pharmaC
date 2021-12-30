d605f1c080227ce810430d3662a6199cbb2e2beb
NevilleJS <nevillejs24@gmail.com>
2021-12-31 00:07:56 +0530

    added report page stuff

--- reportpage.py
+++ reportpage.py
@@ -1,10 +1,5 @@
-import time
+from main import ReadDB
 from os import system, name
-
-import billingpage
-import inventorypage
-import userhistorypage
-import reportpage
 
 def Clear():
     if name == 'nt': _ = system('cls')
@@ -27,53 +22,57 @@ def tabulate(header,data,printheader=True,linesbetweenrows=False,prependspace=0)
         if linesbetweenrows:
             print(prependspace*' '+'+'+'-'*(len(formatted_row.format(*header))+2)+'+')
     if linesbetweenrows == False: print(prependspace*' '+'+'+'-'*(len(formatted_row.format(*header))+2)+'+')
+    
+def BarGrapher(data_):
+    Clear()
+    data = {}
+    maxlen = max(len(i) for i in data_)
+    mindata = min(data_.values())
+    maxdata = max(data_.values())
+    for i in data_:
+        data[i] = int((74-maxlen)*(data_[i]-mindata+1)/(maxdata-mindata+1))
+        if data[i] <= 0:
+            data[i] = 1
+    # print('+'+'-'*80+'+')
+    print('+'+'-'*(maxlen+2)+'+'+(77-maxlen)*'-'+'+')
+    for i in data:
+        print('| '+i.rjust(maxlen)+' | '+"█"*data[i]+" "*(76-maxlen-data[i])+'|')
+        print('+'+'-'*(maxlen+2)+'+'+(77-maxlen)*'-'+'+')
+    input("Press Enter to continue.")
 
 
-def LoadingScreen():
-    Prepend = ' '*8
-    for i in range(58):
-        print(f"""
-{Prepend} ____   _____  ____      ____   _
-{Prepend}/ ___| | ____||  _ \    |  _ \ | |__    __ _  _ __  _ __ ___    __ _
-{Prepend}\___ \ |  _|  | | | |   | |_) || '_ \  / _` || '__|| '_ ` _ \  / _` |
-{Prepend} ___) || |___ | |_| |   |  __/ | | | || (_| || |   | | | | | || (_| |
-{Prepend}|____/ |_____||____/    |_|    |_| |_| \__,_||_|   |_| |_| |_| \__,_|\n\n""")
-        print(f"{Prepend}LOADING: [{'#'*i+' '*(58-i)}]")
-        time.sleep(0.05)
-        Clear()
-
-def HomePage(db):
+def MainPage():
+    temp_db={}
+    userdb = ReadDB('USERS.DB')
+    for i in userdb.items():
+        for j in i[1].items():
+            for k in j[1].items():
+                for l in k[1][:-1]:
+                    if l[1] in temp_db:
+                        temp_db[l[1]] += l[-1]
+                    else:
+                        temp_db.update({l[1]:l[-1]})
+    Clear()
     def takeinput():
         x = input("❯ ")
-        if x in "1 2 3 4 5 0".split():
+        if x in "1 2 0".split():
             if x == '1':
-                Clear()
-                billingpage.FinalEditOption(db)
+                BarGrapher(temp_db)
             if x == '2':
-                inventorypage.MainPage(db)
-            if x == '3':
-                # ManagementPage()
                 pass
-            if x == '4':
-                userhistorypage.MainPage()
-            if x == '5':
-                reportpage.MainPage()
             if x == '0':
                 return -1
         else:
             print("Invalid syntax, Try again:")
             takeinput()
     while True:
-        print("Type the option you want to choose and press Enter [0-4]:")
+        print("Type the option you want to choose and press Enter [0-2]:")
         tabulate(
             "Option Service".split(),
             [
-                "1 Billing".split(),
-                "2 Inventory".split(),
-                "3 Management".split(),
-                "4,User History".split(','),
-                "5 Reports".split(),
-                "0 Quit".split(),
+                "1,Drug sales chart".split(','),
+                "2,Sales Report".split(','),
+                "0,Go back".split(','),
             ],
             linesbetweenrows=True,
         )
