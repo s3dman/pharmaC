2de85f858216efdf9a5b0f780daa418c50c0064e
NevilleJS <nevillejs24@gmail.com>
2022-01-15 11:24:06 +0530

    'burh;

--- run.py
+++ run.py
@@ -1,21 +1,8 @@
-from main import *
-from billingpage import *
-from management import *
-from stock import *
-from ui import *
-from users import *
-from authentication import Login
+from init import initialize
+import os
+if os.name == 'nt':
+    print("Program is running in another cmd window.")
+    os.system('start cmd /K py initialize.py')
+else:
+    initialize()
 
-counter = 5
-while counter>0:
-    if Login() != -1:
-        Clear()
-        db = ReadDB('STOCK.DB')
-        # LoadingScreen()
-        HomePage(db)
-        break
-    else:
-        counter -= 1
-        if counter != 0:
-            print(f"Password incorrect! {counter} attempts remaining.")
-    
