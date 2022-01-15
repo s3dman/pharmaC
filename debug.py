78ec066e788f7d71e450d3223b3d25d72f8bc801
NevilleJS <nevillejs24@gmail.com>
2022-01-15 22:49:31 +0530

    rbuh

--- debug.py
+++ debug.py
@@ -1,17 +1,8 @@
 # debug script for running unit tests
 
-from pprint import pprint
-
 from main import ReadDB, WriteDB
 
-userdb = ReadDB('USERS.DB')
-managementdb = ReadDB('MANAGEMENT.DB')
-stockdb = ReadDB('STOCK.DB')
-# WriteDB({},'USERS.DB')
-# WriteDB({},'STOCK.DB')
-WriteDB({'tax':0,'password':'�100�114�111�119�115�115�97�112'},'MANAGEMENT.DB')
-managementdb = ReadDB('MANAGEMENT.DB')
-
-# pprint(userdb)
-pprint(managementdb)
-pprint(stockdb)
+def setupnew():
+    WriteDB({},'USERS.DB')
+    WriteDB({},'STOCK.DB')
+    WriteDB({'tax':0,'password':'�100�114�111�119�115�115�97�112'},'MANAGEMENT.DB')
