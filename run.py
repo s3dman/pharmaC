a8b24cbc8943f4be664662df4617a8464d9a6975
NevilleJS <nevillejs24@gmail.com>
2022-01-17 21:35:41 +0530

    burh

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ import os
 # checks if windows system and runs the program in a cmd instance
 if os.name == 'nt':
     print("Program is running in another cmd window.")
-    os.system('start cmd /K py initialize.py')
+    os.system('start cmd /K py init.py')
 else:
     init.initialize()
 # if its unix/unix-like system it runs it in the current shell session
